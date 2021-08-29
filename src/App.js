c1bef6157f9532060a7953f070bb4dbbb8f132bb
NAPTheDev <napthedevhcj@gmail.com>
2021-08-29 10:11:28 +0700

    react lazy load with topbar-progress-indicator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import { useState, useEffect, createContext, useMemo } from "react";
+import { useState, useEffect, createContext, useMemo, lazy, Suspense } from "react";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { useTransition, animated } from "react-spring";
 import { useMediaQuery, CssBaseline, CircularProgress } from "@material-ui/core";
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import axios from "axios";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
+import TopBarProgress from "react-topbar-progress-indicator";
+
 import NotFound from "./components/NotFound";
 import Cookie from "./components/Cookie";
 
-import FormsRoute from "./components/forms/FormsRoute";
-import DriveRoute from "./components/drive/DriveRoute";
-import TranslateRoute from "./components/translate/TranslateRoute";
-import MapsRoute from "./components/maps/MapsRoute";
-import DocsRoute from "./components/docs/DocsRoute";
 import { allApps } from "./components/Navbar";
+
+const SignUp = lazy(() => import("./components/SignUp"));
+const SignIn = lazy(() => import("./components/SignIn"));
+const FormsRoute = lazy(() => import("./components/forms/FormsRoute"));
+const DriveRoute = lazy(() => import("./components/drive/DriveRoute"));
+const TranslateRoute = lazy(() => import("./components/translate/TranslateRoute"));
+const MapsRoute = lazy(() => import("./components/maps/MapsRoute"));
+const DocsRoute = lazy(() => import("./components/docs/DocsRoute"));
 
 export const userContext = createContext(null);
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
+
+TopBarProgress.config({
+  barColors: {
+    0: "#0D6EFD",
+    1: "#0D6EFD",
+  },
+});
 
 function App() {
   useEffect(() => {
@@ -95,29 +105,31 @@ function App() {
             <>
               {transitions((props, item) => (
                 <animated.div style={props} className="root">
-                  <Switch location={item}>
-                    <Route path="/" exact>
-                      {allApps.map((e) => (
-                        <Link key={e.name} to={e.route}>
-                          {e.name}
-                        </Link>
-                      ))}
-                    </Route>
-                    <Route path="/sign-in">
-                      <SignIn />
-                    </Route>
-                    <Route path="/sign-up">
-                      <SignUp />
-                    </Route>
-                    <Route path="/forms" component={FormsRoute}></Route>
-                    <Route path="/drive" component={DriveRoute}></Route>
-                    <Route path="/translate" component={TranslateRoute}></Route>
-                    <Route path="/maps" component={MapsRoute}></Route>
-                    <Route path="/docs" component={DocsRoute}></Route>
-                    <Route>
-                      <NotFound />
-                    </Route>
-                  </Switch>
+                  <Suspense fallback={<TopBarProgress />}>
+                    <Switch location={item}>
+                      <Route path="/" exact>
+                        {allApps.map((e) => (
+                          <Link key={e.name} to={e.route}>
+                            {e.name}
+                          </Link>
+                        ))}
+                      </Route>
+                      <Route path="/sign-in">
+                        <SignIn />
+                      </Route>
+                      <Route path="/sign-up">
+                        <SignUp />
+                      </Route>
+                      <Route path="/forms" component={FormsRoute}></Route>
+                      <Route path="/drive" component={DriveRoute}></Route>
+                      <Route path="/translate" component={TranslateRoute}></Route>
+                      <Route path="/maps" component={MapsRoute}></Route>
+                      <Route path="/docs" component={DocsRoute}></Route>
+                      <Route>
+                        <NotFound />
+                      </Route>
+                    </Switch>
+                  </Suspense>
                 </animated.div>
               ))}
             </>

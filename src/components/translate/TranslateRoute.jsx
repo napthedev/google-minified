eb761a78b1dbfad85f6ffd0ea195ece50806e8bb
NAPTheDev <napthedevhcj@gmail.com>
2021-08-30 23:28:05 +0700

    get languages list in translate

--- src/components/translate/TranslateRoute.jsx
+++ src/components/translate/TranslateRoute.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect, useRef } from "react";
 
-import { IconButton, FormControl, Select, MenuItem, Snackbar, TextareaAutosize, Typography } from "@material-ui/core";
+import { IconButton, FormControl, Select, MenuItem, Snackbar, TextareaAutosize, Typography, CircularProgress } from "@material-ui/core";
 import { SwapHoriz, FileCopy, Close } from "@material-ui/icons";
 
 import { copyToClipboard } from "../Functions";
 
 import Navbar, { allApps } from "../Navbar";
-
-const languages = { en: "English", vi: "Vietnamese", ar: "Arabic", zh: "Chinese", fr: "French", de: "German", hi: "Hindi", id: "Indonesian", ga: "Irish", it: "Italian", ja: "Japanese", ko: "Korean", pl: "Polish", pt: "Portuguese", ru: "Russian", es: "Spanish", tr: "Turkish" };
 
 function TranslateRoute() {
   useEffect(() => {
@@ -19,6 +17,7 @@ function TranslateRoute() {
   const [languageTo, setLanguageTo] = useState(localStorage.getItem("languageTo") ? localStorage.getItem("languageTo") : "vi");
   const [inputValue, setInputValue] = useState("");
   const [data, setData] = useState("");
+  const [languages, setLanguages] = useState([]);
 
   const [ellipsis, setEllipsis] = useState(0);
 
@@ -31,6 +30,32 @@ function TranslateRoute() {
     const interval = setInterval(() => {
       setEllipsis((prev) => ++prev % 3);
     }, 300);
+
+    fetch("https://libretranslate.de/languages")
+      .then((res) => res.json())
+      .then((data) => setLanguages(data))
+      .catch((err) => {
+        console.log(err);
+        setLanguages([
+          { code: "en", name: "English" },
+          { code: "ar", name: "Arabic" },
+          { code: "zh", name: "Chinese" },
+          { code: "fr", name: "French" },
+          { code: "de", name: "German" },
+          { code: "hi", name: "Hindi" },
+          { code: "id", name: "Indonesian" },
+          { code: "ga", name: "Irish" },
+          { code: "it", name: "Italian" },
+          { code: "ja", name: "Japanese" },
+          { code: "ko", name: "Korean" },
+          { code: "pl", name: "Polish" },
+          { code: "pt", name: "Portuguese" },
+          { code: "ru", name: "Russian" },
+          { code: "es", name: "Spanish" },
+          { code: "tr", name: "Turkish" },
+          { code: "vi", name: "Vietnamese" },
+        ]);
+      });
 
     return () => clearInterval(interval);
   }, []);
@@ -86,88 +111,94 @@ function TranslateRoute() {
   }, [languageFrom, languageTo, inputValue]);
 
   return (
-    <div>
+    <>
       <Navbar name="Translate" />
 
-      <div className="translate-container">
-        <div className="translate-header">
-          <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
-            <p className="responsive-label">Translate from: </p>
-            <FormControl>
-              <Select
-                value={languageFrom}
-                onChange={(e) => {
-                  if (e.target.value === languageTo) setLanguageTo(languageFrom);
-                  setLanguageFrom(e.target.value);
-                }}
-              >
-                {Object.keys(languages).map((e) => (
-                  <MenuItem key={e} value={e}>
-                    {languages[e]}
-                  </MenuItem>
-                ))}
-                <MenuItem value={"auto"}>Auto Detect</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-          <IconButton
-            onClick={() => {
-              setLanguageFrom(languageTo);
-              setLanguageTo(languageFrom);
-              setInputValue(data ? data : "");
-              setData("");
-            }}
-            className="swap-button"
-            disabled={languageFrom === "auto"}
-          >
-            <SwapHoriz />
-          </IconButton>
-          <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
-            <p className="responsive-label">Translate to: </p>
-            <FormControl>
-              <Select
-                value={languageTo}
-                onChange={(e) => {
-                  if (e.target.value === languageFrom) setLanguageFrom(languageTo);
-                  setLanguageTo(e.target.value);
-                }}
-              >
-                {Object.keys(languages).map((e) => (
-                  <MenuItem key={e} value={e}>
-                    {languages[e]}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </div>
-        </div>
-        <div className="translate-flex">
-          <div className="translate-box-container">
-            <TextareaAutosize
-              className="translate-box"
-              placeholder="Text..."
-              value={inputValue}
-              onChange={(e) => {
-                if (e.target.value.length <= 1000) setInputValue(e.target.value);
+      {languages.length > 0 ? (
+        <div className="translate-container">
+          <div className="translate-header">
+            <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
+              <p className="responsive-label">Translate from: </p>
+              <FormControl>
+                <Select
+                  value={languageFrom}
+                  onChange={(e) => {
+                    if (e.target.value === languageTo) setLanguageTo(languageFrom);
+                    setLanguageFrom(e.target.value);
+                  }}
+                >
+                  {languages.map((e) => (
+                    <MenuItem key={e.code} value={e.code}>
+                      {e.name}
+                    </MenuItem>
+                  ))}
+                  <MenuItem value="auto">Auto Detect</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <IconButton
+              onClick={() => {
+                setLanguageFrom(languageTo);
+                setLanguageTo(languageFrom);
+                setInputValue(data ? data : "");
+                setData("");
               }}
-            />
-            {inputValue.trim() && (
-              <IconButton style={{ position: "absolute", top: 12, right: 7 }} onClick={() => setInputValue("")}>
-                <Close />
-              </IconButton>
-            )}
-            <Typography style={{ position: "absolute", bottom: 5, right: 10 }}>{inputValue.length} / 1000</Typography>
+              className="swap-button"
+              disabled={languageFrom === "auto"}
+            >
+              <SwapHoriz />
+            </IconButton>
+            <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
+              <p className="responsive-label">Translate to: </p>
+              <FormControl>
+                <Select
+                  value={languageTo}
+                  onChange={(e) => {
+                    if (e.target.value === languageFrom) setLanguageFrom(languageTo);
+                    setLanguageTo(e.target.value);
+                  }}
+                >
+                  {languages.map((e) => (
+                    <MenuItem key={e.code} value={e.code}>
+                      {e.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
           </div>
-          <div className="translate-box-container">
-            <div className="translate-box">{data === null ? "Translating" + ".".repeat(ellipsis + 1) : data}</div>
-            {data && (
-              <IconButton style={{ position: "absolute", bottom: 7, right: 7 }} onClick={handleCopy}>
-                <FileCopy />
-              </IconButton>
-            )}
+          <div className="translate-flex">
+            <div className="translate-box-container">
+              <TextareaAutosize
+                className="translate-box"
+                placeholder="Text..."
+                value={inputValue}
+                onChange={(e) => {
+                  if (e.target.value.length <= 1000) setInputValue(e.target.value);
+                }}
+              />
+              {inputValue.trim() && (
+                <IconButton style={{ position: "absolute", top: 12, right: 7 }} onClick={() => setInputValue("")}>
+                  <Close />
+                </IconButton>
+              )}
+              <Typography style={{ position: "absolute", bottom: 5, right: 10 }}>{inputValue.length} / 1000</Typography>
+            </div>
+            <div className="translate-box-container">
+              <div className="translate-box">{data === null ? "Translating" + ".".repeat(ellipsis + 1) : data}</div>
+              {data && (
+                <IconButton style={{ position: "absolute", bottom: 7, right: 7 }} onClick={handleCopy}>
+                  <FileCopy />
+                </IconButton>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="center-container">
+          <CircularProgress />
+        </div>
+      )}
 
       <Snackbar
         anchorOrigin={{
@@ -179,7 +210,7 @@ function TranslateRoute() {
         onClose={() => setSnackbarOpened(false)}
         message="Text copied to clipboard!"
       />
-    </div>
+    </>
   );
 }
 
